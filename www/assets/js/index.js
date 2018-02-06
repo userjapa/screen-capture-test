478bbad9feef6b9d7004a4ef1bdd9a8401180898
usuarioJapa <willian.hsimabukuro@gmail.com>
2018-02-06 09:29:16 -0200

    add comment

--- www/assets/js/index.js
+++ www/assets/js/index.js
@@ -1,22 +1,27 @@
 const screen = document.getElementById('screen')
 
+
+// SUCCESS HANDLER
 const success = stream => {
   screen.src = window.URL.createObjectURL(stream)
   screen.play()
 }
 
+// ERROR HANDLER
 const error = err => {
   console.log('Failed to capture Screen', err)
 }
 
+// CAPTURE SCREEN
 const captureScreen = constraint => {
   navigator.mediaDevices.getUserMedia(constraint)
     .then(success)
     .catch(error)
 }
 
-let constraint = { video: null}
+let constraint = { video: null }
 
+// GET STREAM_ID FOR CHROME
 const getStreamId = (extensionId, req) => {
   return new Promise((resolve, reject) => {
     chrome.runtime.sendMessage(extensionId, req, response => {
@@ -37,20 +42,22 @@ const getStreamId = (extensionId, req) => {
 }
 
 (async () => {
-  if (navigator.mediaDevices.getSupportedConstraints().mediaSource) {
-    if ('getUserMedia' in navigator.mediaDevices) {
-      constraint.video = { mediaSource: 'window' }
+  if ('getUserMedia' in navigator.mediaDevices) {
+    if (navigator.mediaDevices.getSupportedConstraints().mediaSource && typeof InstallTrigger !== 'undefined') { //CHECK IF SUPPORTS mediaSource AND IF IT IS from Firefox
+      constraint.video = { mediaSource: 'screen' }
+    } else if (window.chrome && window.chrome.webstore) { // CHECK IF IT IS FROM Chrome
+      const EXTENSION_ID = 'meiohbiickddgmgomjpnpcjemkdgbkmg'
+      const request = {
+        sources: ['window' ,'screen', 'tab']
+      }
+      try {
+        await getStreamId(EXTENSION_ID, request)
+      } catch (err) {
+        console.log(err)
+      }
     }
+    captureScreen(constraint)
   } else {
-    const EXTENSION_ID = 'meiohbiickddgmgomjpnpcjemkdgbkmg'
-    const request = {
-      sources: ['window' ,'screen', 'tab']
-    }
-    try {
-      await getStreamId(EXTENSION_ID, request)
-    } catch (err) {
-      console.log(err)
-    }
+    alert('Media Devies is not supported')
   }
-  captureScreen(constraint)
 })()
